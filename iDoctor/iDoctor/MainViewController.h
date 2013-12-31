34f05b51919a396091c435f40d226357598b92e9
ddtabakova <dobrinkatabakova@yahoo.com>
2013-12-31 12:22:49 +0200

    Merge branch 'master' of https://github.com/StanchoBancho/iDoctor
    Conflicts:
    	iDoctor/iDoctor.xcodeproj/project.pbxproj

---
+++ iDoctor/iDoctor/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  iDoctor
+//
+//  Created by Stanimir Nikolov on 12/31/13.
+//  Copyright (c) 2013 Stanimir Nikolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
