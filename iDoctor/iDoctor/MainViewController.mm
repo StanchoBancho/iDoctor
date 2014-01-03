ffa98ce18159b8471acfcbc8247bdc696b89c4e9
Stanimir Nikolov <stanimir.nikolov@mentormate.com>
2014-01-03 19:04:29 +0200

    Some more progress with the presenting of autocompletion.

--- iDoctor/iDoctor/MainViewController.mm
+++ iDoctor/iDoctor/MainViewController.mm
@@ -11,18 +11,27 @@
 #import <CoreData/CoreData.h>
 #import "Medicine.h"
 #import "TwoThreeTree.h"
-
+#import "MedicineDetailViewController.h"
 
 @interface MainViewController ()<UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate>
 {
     TwoThreeTree* tree;
 }
 @property (nonatomic, strong) CoreDataManager* sharedManager;
-@property (nonatomic, strong) UITableView* tableView;
-@property (nonatomic, strong) NSMutableArray* medicineNames;
+
+@property (nonatomic, strong) NSMutableArray* suggestedMedicineNames;
+@property (nonatomic, strong) NSMutableArray* choosedMedicineNames;
+@property (nonatomic, strong) NSMutableString* typedText;
+
+@property (nonatomic, strong) IBOutlet UITableView* suggestionsTableView;
+@property (nonatomic, strong) IBOutlet UITableView* tableView;
+@property (nonatomic, strong) IBOutlet UITextField* textField;
+
 @end
 
 @implementation MainViewController
+
+#pragma mark - view lifecycle
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -36,12 +45,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.medicineNames = [NSMutableArray array];
+    self.choosedMedicineNames = [NSMutableArray array];
+    self.typedText = [[NSMutableString alloc] init];
     self.sharedManager = [CoreDataManager sharedManager];
     [self.sharedManager setupDocument:^(UIManagedDocument *document, NSError *error) {
         [self loadTree];
     }];
-	// Do any additional setup after loading the view.
+	
+    [self.suggestionsTableView.layer setCornerRadius:2.0];
+    [self.suggestionsTableView.layer setBorderColor:[[UIColor grayColor] CGColor]];
+    [self.suggestionsTableView.layer setBorderWidth:1.0];
 }
 
 -(void)viewDidAppear:(BOOL)animated
@@ -64,7 +77,7 @@
 {
     // NOTE: if str is nil this will produce an empty C++ string
     // instead of dereferencing the NULL pointer from UTF8String.
-
+    
     //fetch all medicines
     NSManagedObjectContext* context = self.sharedManager.document.managedObjectContext;
     NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Medicine" inManagedObjectContext:context];
@@ -89,7 +102,31 @@
     }
 }
 
+#pragma marl - Autocompletion methods
+
+-(void)showApropriateSuggestionsForString:(NSString*)typedText
+{
+    if([typedText isEqualToString:@""]){
+        [self.suggestionsTableView setHidden:YES];
+    }
+    else{
+        [self.suggestionsTableView setHidden:NO];
+    }
+    
+    //get the suggestion strings for typedText and put them in the self.suggestedMedicineNames
+    
+    [self.suggestionsTableView reloadData];
+}
+
 #pragma mark - UITextFieldDelegate methods
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    [self.typedText replaceCharactersInRange:range withString:string];
+    [self showApropriateSuggestionsForString:self.typedText];
+    
+    return YES;
+}
 
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
@@ -104,6 +141,8 @@
     }
 }
 
+
+
 #pragma mark - UITableView data source
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -113,24 +152,88 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.medicineNames.count;
+    if (tableView == self.suggestionsTableView) {
+        return self.suggestedMedicineNames.count;
+    }
+    else{
+        return self.choosedMedicineNames.count;
+    }
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"MedicineCell"];
-    NSString* medicineTitle = self.medicineNames[indexPath.row];
-    cell.textLabel.text = medicineTitle;
-    [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
-    return cell;
+    
+    if(tableView == self.suggestionsTableView){
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"MedicineCell"];
+        NSString* medicineTitle = self.suggestedMedicineNames[indexPath.row];
+        cell.textLabel.text = medicineTitle;
+        return cell;
+    }
+    else
+    {
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"MedicineCell"];
+        NSString* medicineTitle = [self.choosedMedicineNames[indexPath.row] objectForKey:@"name"];
+        cell.textLabel.text = medicineTitle;
+        BOOL isExisting = [[self.choosedMedicineNames[indexPath.row] objectForKey:@"isExisting"] boolValue];
+        if (isExisting) {
+            [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+        }
+        else{
+            [cell setAccessoryType:UITableViewCellAccessoryNone];
+        }
+        return cell;
+    }
 }
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(tableView == self.tableView){
+        BOOL isExisting = [[self.choosedMedicineNames[indexPath.row] objectForKey:@"isExisting"] boolValue];
+        if (isExisting) {
+            //fetch the existing medicine
+            NSManagedObjectContext* context = self.sharedManager.document.managedObjectContext;
+            NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Medicine" inManagedObjectContext:context];
+            NSFetchRequest *request = [[NSFetchRequest alloc] init];
+            [request setEntity:entityDescription];
+            NSString* medicineTitle = [self.choosedMedicineNames[indexPath.row] objectForKey:@"name"];
+            [request setPredicate:[NSPredicate predicateWithFormat:@"name like %@", medicineTitle]];
+            NSError *error;
+            NSArray *array = [context executeFetchRequest:request error:&error];
+            if (error || array == nil || array.count < 1){
+                NSLog(@"GOLQM ERROR :%@", error);
+            }
+            else{
+                Medicine * selectedMedicine = [array objectAtIndex:0];
+                if(selectedMedicine && selectedMedicine.descriptionUrl){
+                    MedicineDetailViewController* detailViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"MedicineDetailViewController"];
+                    [detailViewController setMedicineUrl:selectedMedicine.descriptionUrl];
+                }
+            }
+        }
+    }
+    else{
+        //chose selected medicine
+        NSString* medicineTitle = self.suggestedMedicineNames[indexPath.row];
+        NSDictionary* newObject = @{@"name":medicineTitle, @"isExisting":@NO};
+        [self.choosedMedicineNames insertObject:newObject atIndex:0];
+        [self.textField setText:@""];
+        [self.tableView reloadData];
+    }
+}
+
+
 
 #pragma mark - Action methods
 
 -(IBAction)addButtonPressed:(id)sender
 {
-
-
+    NSCharacterSet* whiteSpaces = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    NSString* typedText = [self.textField.text stringByTrimmingCharactersInSet:whiteSpaces];
+    if (![typedText isEqualToString:@""]) {
+        NSDictionary* newObject = @{@"name":typedText, @"isExisting":@NO};
+        [self.choosedMedicineNames insertObject:newObject atIndex:0];
+        [self.textField setText:@""];
+    }
 }
 
 @end
